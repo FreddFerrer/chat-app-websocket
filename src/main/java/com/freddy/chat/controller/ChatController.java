cdea53e87354e4b8b2f6ac0e6be71aaa77fd19dc
FreddyWhitDoubleD <ferreirafreddy39@gmail.com>
2023-12-30 12:02:40 -0300

    model and controller added

---
+++ src/main/java/com/freddy/chat/controller/ChatController.java
@@ -0,0 +1,23 @@
+package com.freddy.chat.controller;
+
+import com.freddy.chat.model.Mensaje;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import java.util.Date;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/mensaje")   //envia el mensaje
+    @SendTo("/chat/mensaje")        //recibe el mensaje o "escucha"
+    public Mensaje recibeMensaje(Mensaje mensaje) {
+        mensaje.setFecha(new Date().getTime());
+        mensaje.setTexto("Recibido por el broker: " + mensaje.getTexto());
+
+        return mensaje;
+    }
+
+
+}
